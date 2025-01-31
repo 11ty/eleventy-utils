9c54ad0782863bc9a23093914902b0cb09b18d71
Zach Leatherman <zachleat@users.noreply.github.com>
2025-01-31 11:22:57 -0600

    Adds DateCompare utility for use by Fetch and Import for cache durations

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TemplatePath = require("./src/TemplatePath.js");
 const isPlainObject = require("./src/IsPlainObject.js");
 const Merge = require("./src/Merge.js");
+const DateCompare = require("./src/DateCompare.js");
 const { DeepCopy } = Merge;
 
 module.exports = {
@@ -8,4 +9,5 @@ module.exports = {
   isPlainObject,
   Merge,
   DeepCopy,
+  DateCompare,
 };
