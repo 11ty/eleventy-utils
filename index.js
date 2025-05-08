6aa9b76e937a49c3a51eac9fc258d7fa314100f7
Zach Leatherman <zachleat@users.noreply.github.com>
2025-05-08 12:36:10 -0500

    Add exports

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const isPlainObject = require("./src/IsPlainObject.js");
 const Merge = require("./src/Merge.js");
 const DateCompare = require("./src/DateCompare.js");
 const { DeepCopy } = Merge;
-const { createHash, createHashHex } = require("./src/CreateHash.js");
+const { createHash, createHashHex, createHashSync, createHashHexSync } = require("./src/CreateHash.js");
 const Buffer = require("./src/Buffer.js");
 
 module.exports = {
@@ -14,5 +14,7 @@ module.exports = {
   DateCompare,
   createHash,
   createHashHex,
+  createHashSync,
+  createHashHexSync,
   Buffer,
 };
