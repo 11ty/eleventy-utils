81273dc464a2f8f11800e9e9bb2a40687fb40789
Zach Leatherman <zachleat@users.noreply.github.com>
2025-05-08 12:20:59 -0500

    Refactor, add direct script option for sha256 hashing

--- src/CreateHash.js
+++ src/CreateHash.js
@@ -1,69 +1,27 @@
 
-const { base64UrlSafe } = require("./Url.js");
-const { isBuffer } = require("./Buffer.js");
-
-function toBase64(bytes) {
-	let str = Array.from(bytes, (b) => String.fromCodePoint(b)).join("");
-
-	// `btoa` Node 16+
-	return btoa(str);
-}
-
-// Thanks https://evanhahn.com/the-best-way-to-concatenate-uint8arrays/ (Public domain)
-function mergeUint8Array(...arrays) {
-  let totalLength = arrays.reduce(
-    (total, uint8array) => total + uint8array.byteLength,
-    0
-  );
-
-  let result = new Uint8Array(totalLength);
-  let offset = 0;
-  arrays.forEach((uint8array) => {
-    result.set(uint8array, offset);
-    offset += uint8array.byteLength;
-  });
-
-  return result;
-}
-
-async function digestHash(...content) {
-	let encoder = new TextEncoder();
-	let input = mergeUint8Array(...content.map(c => {
-		if(isBuffer(c)) {
-			return c;
-		}
-		return encoder.encode(c);
-	}));
-
-	// `crypto` is Node 20+
-	return crypto.subtle.digest("SHA-256", input);
-}
+const { Hash } = require("./HashTypes.js");
 
 // same output as node:crypto above (though now async).
 async function createHash(...content) {
-	if(typeof globalThis.crypto === "undefined") {
-		// Backwards compat with Node Crypto, since WebCrypto (crypto global) is Node 20+
-		const { createHash } = require("./CreateHash-Node.js");
-		return createHash(...content);
-	}
-
-	return digestHash(...content).then(hashBuffer => {
-		return base64UrlSafe(toBase64(new Uint8Array(hashBuffer)));
-	});
+	return Hash.create().toBase64Url(...content);
 }
 
 async function createHashHex(...content) {
-	if(typeof globalThis.crypto === "undefined") {
-		// Backwards compat with Node Crypto, since WebCrypto (crypto global) is Node 20+
-		const { createHashHex } = require("./CreateHash-Node.js");
-		return createHashHex(...content);
-	}
-
-	return digestHash(...content).then(hashBuffer => {
-  	return Array.from(new Uint8Array(hashBuffer))
-	    .map((b) => b.toString(16).padStart(2, "0"))
-	    .join("");
-	});
+	return Hash.create().toHex(...content);
 }
 
-module.exports = { createHash, createHashHex };
+// Slower, but this feature does not require WebCrypto
+function createHashSync(...content) {
+	return Hash.createSync().toBase64Url(...content);
+}
+
+function createHashHexSync(...content) {
+	return Hash.createSync().toHex(...content);
+}
+
+module.exports = {
+	createHash,
+	createHashSync,
+	createHashHex,
+	createHashHexSync,
+};
--- test/CreateHashTest.js
+++ test/CreateHashTest.js
@@ -2,8 +2,10 @@ const assert = require("node:assert/strict")
 const test = require("node:test");
 const fs = require("node:fs");
 
-const { createHash, createHashHex } = require("../src/CreateHash.js");
-const { createHash: createHashNodeCrypto, createHashHex: createHashNodeCryptoHex } = require("../src/CreateHash-Node.js");
+const { createHash, createHashHex, createHashSync, createHashHexSync } = require("../src/CreateHash.js");
+
+// To test individual types together
+const { NodeCryptoHash, ScriptHash, WebCryptoHash } = require("../src/HashTypes.js");
 
 test("Basic usage", async (t) => {
 	const emptyHash = await createHash("");
@@ -13,6 +15,16 @@ test("Basic usage", async (t) => {
 	assert.equal(emptyHash.includes("="), false);
 
 	assert.equal(await createHash("This is a test"), "x74e2QL7jdTUiZfGRS9dflCfvNvigIsWvPTtzkwH0U4");
+});
+
+test("Basic usage (sync)", async (t) => {
+	const emptyHash = createHashSync("");
+	assert.equal(emptyHash, "47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU");
+	assert.equal(emptyHash.includes("/"), false);
+	assert.equal(emptyHash.includes("+"), false);
+	assert.equal(emptyHash.includes("="), false);
+
+	assert.equal(createHashSync("This is a test"), "x74e2QL7jdTUiZfGRS9dflCfvNvigIsWvPTtzkwH0U4");
 });
 
 test("Basic usage (hex)", async (t) => {
@@ -25,28 +37,93 @@ test("Basic usage (hex)", async (t) => {
 	assert.equal(await createHashHex("This is a test"), "c7be1ed902fb8dd4d48997c6452f5d7e509fbcdbe2808b16bcf4edce4c07d14e");
 });
 
-test("Multiple calls", async (t) => {
-	assert.equal(await createHash(), createHashNodeCrypto());
-	assert.equal(await createHash(""), createHashNodeCrypto(""));
-	assert.equal(await createHash("a", "b"), createHashNodeCrypto("a", "b"));
-	assert.equal(await createHash("a", "b", "c"), createHashNodeCrypto("a", "b", "c"));
-	assert.equal(await createHash("abcdef", "def"), createHashNodeCrypto("abcdef", "def"));
+test("Basic usage (sync, hex)", async (t) => {
+	const emptyHash = createHashHexSync("");
+	assert.equal(emptyHash, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+	assert.equal(emptyHash.includes("/"), false);
+	assert.equal(emptyHash.includes("+"), false);
+	assert.equal(emptyHash.includes("="), false);
+
+	assert.equal(createHashHexSync("This is a test"), "c7be1ed902fb8dd4d48997c6452f5d7e509fbcdbe2808b16bcf4edce4c07d14e");
 });
 
-test("Multiple calls (hex)", async (t) => {
-	assert.equal(await createHashHex(), createHashNodeCryptoHex());
-	assert.equal(await createHashHex(""), createHashNodeCryptoHex(""));
-	assert.equal(await createHashHex("a", "b"), createHashNodeCryptoHex("a", "b"));
-	assert.equal(await createHashHex("a", "b", "c"), createHashNodeCryptoHex("a", "b", "c"));
-	assert.equal(await createHashHex("abcdef", "def"), createHashNodeCryptoHex("abcdef", "def"));
+// Skip these explicit tests since WebCrypto API is not available
+if(typeof globalThis.crypto !== "undefined") {
+	test("Multiple calls give the same output (WebCrypto <-> Node)", async (t) => {
+		assert.equal(await WebCryptoHash.toBase64Url(), NodeCryptoHash.toBase64Url());
+		assert.equal(await WebCryptoHash.toBase64Url(""), NodeCryptoHash.toBase64Url(""));
+		assert.equal(await WebCryptoHash.toBase64Url("a", "b"), NodeCryptoHash.toBase64Url("a", "b"));
+		assert.equal(await WebCryptoHash.toBase64Url("a", "b", "c"), NodeCryptoHash.toBase64Url("a", "b", "c"));
+		assert.equal(await WebCryptoHash.toBase64Url("abcdef", "def"), NodeCryptoHash.toBase64Url("abcdef", "def"));
+	});
+
+	test("Multiple calls give the same output (WebCrypto <-> Node; hex)", async (t) => {
+		assert.equal(await WebCryptoHash.toHex(), NodeCryptoHash.toHex());
+		assert.equal(await WebCryptoHash.toHex(""), NodeCryptoHash.toHex(""));
+		assert.equal(await WebCryptoHash.toHex("a", "b"), NodeCryptoHash.toHex("a", "b"));
+		assert.equal(await WebCryptoHash.toHex("a", "b", "c"), NodeCryptoHash.toHex("a", "b", "c"));
+		assert.equal(await WebCryptoHash.toHex("abcdef", "def"), NodeCryptoHash.toHex("abcdef", "def"));
+	});
+
+	test("Multiple calls give the same output, Buffer  (WebCrypto <-> Node)", async (t) => {
+		let buffer = fs.readFileSync("./test/stubs/sample.png");
+		assert.equal(await WebCryptoHash.toBase64Url(buffer, "def"), NodeCryptoHash.toBase64Url(buffer, "def"));
+	});
+
+	test("Multiple calls give the same output, Buffer  (WebCrypto <-> Node; hex)", async (t) => {
+		let buffer = fs.readFileSync("./test/stubs/sample.png");
+		assert.equal(await WebCryptoHash.toHex(buffer, "def"), NodeCryptoHash.toHex(buffer, "def"));
+	});
+
+	test("Multiple calls give the same output (WebCrypto <-> Script)", async (t) => {
+		assert.equal(await WebCryptoHash.toBase64Url(), ScriptHash.toBase64Url());
+		assert.equal(await WebCryptoHash.toBase64Url(""), ScriptHash.toBase64Url(""));
+		assert.equal(await WebCryptoHash.toBase64Url("a", "b"), ScriptHash.toBase64Url("a", "b"));
+		assert.equal(await WebCryptoHash.toBase64Url("a", "b", "c"), ScriptHash.toBase64Url("a", "b", "c"));
+		assert.equal(await WebCryptoHash.toBase64Url("abcdef", "def"), ScriptHash.toBase64Url("abcdef", "def"));
+	});
+
+	test("Multiple calls give the same output (WebCrypto <-> Script; hex)", async (t) => {
+		assert.equal(await WebCryptoHash.toHex(), ScriptHash.toHex());
+		assert.equal(await WebCryptoHash.toHex(""), ScriptHash.toHex(""));
+		assert.equal(await WebCryptoHash.toHex("a", "b"), ScriptHash.toHex("a", "b"));
+		assert.equal(await WebCryptoHash.toHex("a", "b", "c"), ScriptHash.toHex("a", "b", "c"));
+		assert.equal(await WebCryptoHash.toHex("abcdef", "def"), ScriptHash.toHex("abcdef", "def"));
+	});
+
+	test("Multiple calls give the same output, Buffer  (WebCrypto <-> Script)", async (t) => {
+		let buffer = fs.readFileSync("./test/stubs/sample.png");
+		assert.equal(await WebCryptoHash.toBase64Url(buffer, "def"), ScriptHash.toBase64Url(buffer, "def"));
+	});
+
+	test("Multiple calls give the same output, Buffer  (WebCrypto <-> Script; hex)", async (t) => {
+		let buffer = fs.readFileSync("./test/stubs/sample.png");
+		assert.equal(await WebCryptoHash.toHex(buffer, "def"), ScriptHash.toHex(buffer, "def"));
+	});
+}
+
+test("Multiple calls give the same output (Node <-> Script)", async (t) => {
+	assert.equal(NodeCryptoHash.toBase64Url(), ScriptHash.toBase64Url());
+	assert.equal(NodeCryptoHash.toBase64Url(""), ScriptHash.toBase64Url(""));
+	assert.equal(NodeCryptoHash.toBase64Url("a", "b"), ScriptHash.toBase64Url("a", "b"));
+	assert.equal(NodeCryptoHash.toBase64Url("a", "b", "c"), ScriptHash.toBase64Url("a", "b", "c"));
+	assert.equal(NodeCryptoHash.toBase64Url("abcdef", "def"), ScriptHash.toBase64Url("abcdef", "def"));
 });
 
-test("Multiple calls, Buffer", async (t) => {
+test("Multiple calls give the same output (Node <-> Script; hex)", async (t) => {
+	assert.equal(NodeCryptoHash.toHex(), ScriptHash.toHex());
+	assert.equal(NodeCryptoHash.toHex(""), ScriptHash.toHex(""));
+	assert.equal(NodeCryptoHash.toHex("a", "b"), ScriptHash.toHex("a", "b"));
+	assert.equal(NodeCryptoHash.toHex("a", "b", "c"), ScriptHash.toHex("a", "b", "c"));
+	assert.equal(NodeCryptoHash.toHex("abcdef", "def"), ScriptHash.toHex("abcdef", "def"));
+});
+
+test("Multiple calls give the same output, Buffer  (Node <-> Script)", async (t) => {
 	let buffer = fs.readFileSync("./test/stubs/sample.png");
-	assert.equal(await createHash(buffer, "def"), createHashNodeCrypto(buffer, "def"));
+	assert.equal(NodeCryptoHash.toBase64Url(buffer, "def"), ScriptHash.toBase64Url(buffer, "def"));
 });
 
-test("Multiple calls, Buffer (hex)", async (t) => {
+test("Multiple calls give the same output, Buffer  (Node <-> Script; hex)", async (t) => {
 	let buffer = fs.readFileSync("./test/stubs/sample.png");
-	assert.equal(await createHashHex(buffer, "def"), createHashNodeCryptoHex(buffer, "def"));
+	assert.equal(NodeCryptoHash.toHex(buffer, "def"), ScriptHash.toHex(buffer, "def"));
 });
